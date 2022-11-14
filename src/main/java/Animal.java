40b5c809990d357f7e0972e7c5a9e87ff65a314a
gottbrecht <simo79j5@stud.kea.dk>
2022-11-14 14:53:34 +0100

    mood metode done

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -5,6 +5,7 @@ public abstract class Animal {
     public abstract void makeSound();
 
 
+    public abstract boolean showMood(boolean angry, boolean happy);
 
     public abstract void showMood(boolean mood);
 }
